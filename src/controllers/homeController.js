f3867b11f172164d18e762c698045e84b9c492c7
Velislav21 <zirovcho34@gmail.com>
2024-10-05 13:26:01 +0300

    add base movie service

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,33 +1,11 @@
 import express from 'express';
 // import { Router }
+import movieServices from '../services/movieServices.js';
 
 const router = express.Router();
 
-const movies = [
-    {
-        id: "1",
-        name: "Jungle Cuise",
-        imageUrl: "/img/jungle-cruise.jpeg",
-        genre: "Adventure",
-        description: 'Descrppt'
-    },
-    {
-        id: "2",
-        name: "The Little Mermaid",
-        imageUrl: "/img/the-little-mermaid.jpg",
-        genre: "Fantasy",
-        description: 'Descrppt12'
-    },
-    {
-        id: "3",
-        name: "Home Alone",
-        imageUrl: "/img/home-alone.jpeg",
-        genre: "Comedy",
-        description: 'Descrpp1212t'
-    }
-]
-
 router.get('/', (req, res) => {
+    const movies = movieServices.getAll();
     res.render('home', { movies })
 })
 router.get('/about', (req, res) => {
---
+++ src/services/movieServices.js
@@ -0,0 +1,29 @@
+const movies = [
+    {
+        id: "1",
+        name: "Jungle Cuise",
+        imageUrl: "/img/jungle-cruise.jpeg",
+        genre: "Adventure",
+        description: 'Descrppt'
+    },
+    {
+        id: "2",
+        name: "The Little Mermaid",
+        imageUrl: "/img/the-little-mermaid.jpg",
+        genre: "Fantasy",
+        description: 'Descrppt12'
+    },
+    {
+        id: "3",
+        name: "Home Alone",
+        imageUrl: "/img/home-alone.jpeg",
+        genre: "Comedy",
+        description: 'Descrpp1212t'
+    }
+]
+
+const getAll = () => movies;
+
+export default {
+    getAll,
+}
