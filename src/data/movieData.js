ac48e8cfbdcb53fbe454d8ece8e9ef073b1887ba
Velislav21 <zirovcho34@gmail.com>
2024-10-05 15:55:49 +0300

    add redirect and create movie

--- src/data/movieData.js
+++ src/data/movieData.js
@@ -1,19 +1,31 @@
 import fs from 'fs/promises';
 import path from 'path';
 
+const dbPath = path.resolve('./src/db.json');
+
 async function getDb() {
-    const dbPath = path.resolve('./src/db.json');
     const jsonResult = await fs.readFile(dbPath, { encoding: 'utf8' });
-    
+
     const data = JSON.parse(jsonResult);
     return data;
 }
 
-async function getMovies() {
+async function getAll() {
     const db = await getDb();
     return db.movies
 }
 
+function saveDb(data) {
+    return fs.writeFile(dbPath, JSON.stringify(data, {}, 2))
+}
+
+async function create(movieData) {
+    const db = await getDb();
+
+    db.movies.push(movieData);
+    return saveDb(db);
+}
+
 export default {
-    getMovies
+    getAll, create
 }
--- src/services/movieServices.js
+++ src/services/movieServices.js
@@ -1,7 +1,13 @@
 import movieData from '../data/movieData.js'
+import uniqid from 'uniqid'
+const getAll = () => movieData.getAll();
 
-const getAll = () => movieData.getMovies();
+const create = (movie) => {
+    movie.id = uniqid();
+    return movieData.create(movie)
+
+}
 
 export default {
-    getAll,
+    getAll, create
 }
