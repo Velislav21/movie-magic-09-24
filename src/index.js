a5f1a753f08bb9157109c91edf77cc2eab527dbc
Velislav21 <zirovcho34@gmail.com>
2024-10-05 11:27:47 +0300

    Set up express
    i had to type ''npm i --save-dev @types/express" to make it work, never needed to do so before, happened right after VSC update.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import express from 'express';
+import express from "express";
 
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('ITS WORKINGGGGGGGG')
+})
+app.listen(5000, () => console.log('server is listening on http://localhost:5000'))
