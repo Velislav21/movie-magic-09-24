e10ece6eae096134d16b65092f406034ec0f557f
Velislav21 <zirovcho34@gmail.com>
2024-10-05 20:11:55 +0300

    minor changes

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,11 +1,41 @@
 import { Router } from "express";
+import movieServices from "../services/movieServices.js";
 
 const router = Router();
 
 
 // URL -> /movies/create
-router.get('create', (req, res) => {
+router.get('/create', (req, res) => {
     res.render('movies/create')
 })
 
+router.post('/create', async (req, res) => {
+    const movieData = req.body;
+
+    await movieServices.create(movieData)
+
+    res.redirect('/')
+})
+
+router.get('/:movieId/details', async (req, res) => {
+
+    const movieId = req.params.movieId
+    const movie = await movieServices.getOne(movieId)
+
+    movie.rating = getRatingView(movie.rating)
+    res.render('movies/details', { movie })
+})
+
+router.get('/search', async (req, res) => {
+    const query = req.query;
+    const movies = await movieServices.getAll(query);
+    res.render('home', { isSearch: true, movies })
+})
+
+function getRatingView(rating) {
+    if (!Number.isInteger(rating)) {
+        return 'n\\a'
+    }
+    return '&#x2605;'.repeat(rating)
+}
 export default router;
