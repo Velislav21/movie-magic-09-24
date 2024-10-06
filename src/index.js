b7f03b7a1cdb2cbeb31a89e27c9d2f99b7a1cc75
Velislav21 <zirovcho34@gmail.com>
2024-10-06 17:49:33 +0300

    extract handlebars and express configuration

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import express from "express";
-import handlebars from 'express-handlebars';
 
 import routes from './routes.js';
+import handlebarsInit from "./config/handlebarsInit.js";
+import expressInit from "./config/expressInit.js";
 
 const app = express();
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs'
-}))
-app.set('view engine', 'hbs')
-app.set('views', './src/views')
+handlebarsInit(app);
+expressInit(app);
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public'))
 app.use(routes)
 
 app.listen(5000, () => console.log('server is listening on http://localhost:5000'))
