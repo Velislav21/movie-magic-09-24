359ae5d782a447343a7f90c5826c8ccdd4211fe7
Velislav21 <zirovcho34@gmail.com>
2024-10-08 17:11:52 +0300

    add movieController functionality

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import movieServices from "../services/movieServices.js";
+import castService from "../services/castService.js";
 
 const router = Router();
 
-function toArray(documents) {
-    return documents.map(document => document.toObject());
-}
 // URL -> /movies/create
 router.get('/create', (req, res) => {
     res.render('movies/create')
@@ -21,28 +19,33 @@ router.post('/create', async (req, res) => {
 
 router.get('/search', async (req, res) => {
     const filter = req.query;
-    const movies = await movieServices.getAll(filter);
-    res.render('home', { isSearch: true, movies: toArray(movies), filter })
+
+    const movies = await movieServices.getAll(filter).lean();
+
+    res.render('home', { isSearch: true, movies, filter })
 })
 
 router.get('/:movieId/details', async (req, res) => {
 
     const movieId = req.params.movieId
-    const movie = await movieServices.getOne(movieId);
+    const movie = await movieServices.getOne(movieId).lean();
 
-    movie.rating = getRatingView(movie.rating)
     res.render('movies/details', { movie })
 })
 
-router.get('/:movieId/attach', (req, res) => {
-    res.render('movies/attach')
+router.get('/:movieId/attach', async (req, res) => {
+    const movie = await movieServices.getOne(req.params.movieId).lean();
+    const casts = await castService.getAllWithout(movie.casts).lean();
+
+    res.render('movies/attach', { movie, casts });
 })
+router.post('/:movieId/attach', async (req, res) => {
+    const movieId = req.params.movieId;
+    const castId = req.body.cast;
+    const character = req.body.character;
 
+    await movieService.attach(movieId, castId, character);
 
-function getRatingView(rating) {
-    if (!Number.isInteger(rating)) {
-        return 'n\\a'
-    }
-    return '&#x2605;'.repeat(rating)
-}
+    res.redirect(`/movies/${movieId}/details`);
+});
 export default router;
